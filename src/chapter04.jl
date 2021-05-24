25451ffe62227a41a569c0ed06ac450350c0866e
Toby Driscoll <driscoll@udel.edu>
2021-05-23 21:00:37 -0400

    facelift for levenberg

--- src/chapter04.jl
+++ src/chapter04.jl
@@ -124,7 +124,7 @@ return J
 end
 
 """
-    levenberg(f,x₁,tol[;maxiter,ftol,xtol])
+    levenberg(f,x₁[;maxiter,ftol,xtol])
 
 Use Levenberg's quasi-Newton iteration to find a root of the system
 `f`, starting from `x₁`, with `tol` as the stopping tolerance in
@@ -138,38 +138,37 @@ and the stopping tolerance for values of `f` and changes in `x`.
 function levenberg(f,x₁;maxiter=40,ftol=1e-12,xtol=1e-12)
 
 x = [float(x₁)]
-fₖ = f(x₁)
+yₖ = f(x₁)
 k = 1;  s = Inf;
-A = fdjac(f,x[k],fₖ)   # start with FD Jacobian
+A = fdjac(f,x[k],yₖ)   # start with FD Jacobian
 jac_is_new = true
 
 λ = 10;
-while (norm(s) > xtol) && (norm(fₖ) > ftol)
+while (norm(s) > xtol) && (norm(yₖ) > ftol)
     # Compute the proposed step.
     B = A'*A + λ*I
-    z = A'*fₖ
+    z = A'*yₖ
     s = -(B\z)
-
-    xnew = x[k] + s
-    fnew = f(xnew)
+    
+    x̂ = x[k] + s
+    ŷ = f(x̂)
 
     # Do we accept the result?
-    if norm(fnew) < norm(fₖ)    # accept
-        push!(x,xnew)
-        y = fnew - fₖ
-        fₖ = fnew
-        k += 1
-
+    if norm(ŷ) < norm(yₖ)    # accept
         λ = λ/10   # get closer to Newton
         # Broyden update of the Jacobian.
-        A += (y-A*s)*(s'/(s'*s))
+        A += (ŷ-yₖ-A*s)*(s'/(s'*s))
         jac_is_new = false
+        
+        push!(x,x̂)
+        yₖ = ŷ
+        k += 1
     else                       # don't accept
-        # Get closer to steepest descent.
+        # Get closer to gradient descent.
         λ = 4λ
         # Re-initialize the Jacobian if it's out of date.
         if !jac_is_new
-            A = fdjac(f,x[k],fₖ)
+            A = fdjac(f,x[k],yₖ)
             jac_is_new = true
         end
     end
