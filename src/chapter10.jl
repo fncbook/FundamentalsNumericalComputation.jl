5e33ed7fbe5dcfb6f717b276018df1dd41e6d13f
Toby Driscoll <driscoll@udel.edu>
2021-07-03 10:38:36 -0400

    cosmetic improvements

--- src/chapter10.jl
+++ src/chapter10.jl
@@ -39,26 +39,27 @@ for the first and second derivatives.
 function diffmat2(n,xspan)
     a,b = xspan
     h = (b-a)/n
-    x = [ a + i*h for i=0:n ]   # nodes
+    x = [ a + i*h for i in 0:n ]   # nodes
 
-    # Define most of Dx by its diagonals.
+    # Define most of Dₓ by its diagonals.
     dp = fill(0.5/h,n)        # superdiagonal
     dm = fill(-0.5/h,n)       # subdiagonal
-    Dx = diagm(-1=>dm,1=>dp)
+    Dₓ = diagm(-1=>dm,1=>dp)
 
     # Fix first and last rows.
-    Dx[1,1:3] = [-1.5,2,-0.5]/h
-    Dx[n+1,n-1:n+1] = [0.5,-2,1.5]/h
+    Dₓ[1,1:3] = [-1.5,2,-0.5]/h
+    Dₓ[n+1,n-1:n+1] = [0.5,-2,1.5]/h
 
-    # Define most of Dxx by its diagonals.
+    # Define most of Dₓₓ by its diagonals.
     d0 =  fill(-2/h^2,n+1)    # main diagonal
-    dp =  ones(n)/h^2         # superdiagonal and subdiagonal
-    Dxx = diagm(-1=>dp,0=>d0,1=>dp)
+    dp =  ones(n)/h^2         # super- and subdiagonal
+    Dₓₓ = diagm(-1=>dp,0=>d0,1=>dp)
 
     # Fix first and last rows.
-    Dxx[1,1:4] = [2,-5,4,-1]/h^2
-    Dxx[n+1,n-2:n+1] = [-1,4,-5,2]/h^2
-    return x,Dx,Dxx
+    Dₓₓ[1,1:4] = [2,-5,4,-1]/h^2
+    Dₓₓ[n+1,n-2:n+1] = [-1,4,-5,2]/h^2
+
+    return x,Dₓ,Dₓₓ
 end
 
 """
@@ -69,26 +70,26 @@ interval `xspan`. Returns a vector of nodes and the matrices for the
 first and second derivatives.
 """
 function diffcheb(n,xspan)
-    x = [ -cos( k*π/n ) for k=0:n ]    # nodes in [-1,1]
-    Dx = zeros(n+1,n+1)
+    x = [ -cos( k*π/n ) for k in 0:n ]    # nodes in [-1,1]
+    
+    # Off-diagonal entries.
     c = [2; ones(n-1); 2];    # endpoint factors
+    dij = (i,j) -> (-1)^(i+j)*c[i+1]/(c[j+1]*(x[i+1]-x[j+1]))
+    Dₓ = [ dij(i,j) for i in 0:n, j in 0:n ]
 
-    # Off-diagonal entries
-    Dx = [ (-1)^(i+j)*c[i+1]/(c[j+1]*(x[i+1]-x[j+1])) for i=0:n, j=0:n ]
+    # Diagonal entries.
+    Dₓ[isinf.(Dₓ)] .= 0         # fix divisions by zero on diagonal
+    s = sum(Dₓ,dims=2)
+    Dₓ -= diagm(s[:,1])         # "negative sum trick"
 
-    # Diagonal entries
-    Dx[isinf.(Dx)] .= 0              # fix divisions by zero on diagonal
-    s = sum(Dx,dims=2)
-    Dx -= diagm(0=>s[:,1])           # "negative sum trick"
-
-    # Transplant to [a,b]
+    # Transplant to [a,b].
     a,b = xspan
     x = @. a + (b-a)*(x+1)/2
-    Dx = 2*Dx/(b-a)
+    Dₓ = 2*Dₓ/(b-a)             # chain rule
 
-    # Second derivative
-    Dxx = Dx^2
-    return x,Dx,Dxx
+    # Second derivative.
+    Dₓₓ = Dₓ^2
+    return x,Dₓ,Dₓₓ
 end
 
 """
@@ -102,11 +103,11 @@ be `n`+1 equally spaced nodes, including the endpoints.
 Returns vectors of the nodes and the solution values.
 """
 function bvplin(p,q,r,xspan,lval,rval,n)
-    x,Dx,Dxx = diffmat2(n,xspan)
+    x,Dₓ,Dₓₓ = diffmat2(n,xspan)
 
     P = diagm(0=>p.(x))
     Q = diagm(0=>q.(x))
-    L = Dxx + P*Dx + Q     # ODE expressed at the nodes
+    L = Dₓₓ + P*Dₓ + Q     # ODE expressed at the nodes
 
     # Replace first and last rows using boundary conditions.
     I = Diagonal(ones(n+1))
@@ -131,13 +132,13 @@ Returns vectors for the nodes and the values of u.
 """
 function bvp(ϕ,xspan,g₁,g₂,init)
     n = length(init) - 1
-    x,Dx,Dxx = diffmat2(n,xspan)
+    x,Dₓ,Dₓₓ = diffmat2(n,xspan)
     h = x[2]-x[1]
 
     function residual(u)
         # Residual of the ODE at the nodes. 
-        du_dx = Dx*u                   # discrete u'
-        d2u_dx2 = Dxx*u                # discrete u''
+        du_dx = Dₓ*u                   # discrete u'
+        d2u_dx2 = Dₓₓ*u                # discrete u''
         f = d2u_dx2 - ϕ.(x,u,du_dx)
 
         # Replace first and last values by boundary conditions.
